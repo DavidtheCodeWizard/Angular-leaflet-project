b8696df38efad7a21cbdf7672252f94627c09a60
David Vreugdenhil <d.vreugdenhil@safetyct.com>
2026-02-02 15:12:08 +0100

    Added Leaflet with search feature

--- src/app/pages/map/map.ts
+++ src/app/pages/map/map.ts
@@ -1,14 +1,17 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-map',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './map.html',
   styleUrl: './map.scss',
 })
 export class MapComponent implements AfterViewInit {
   private map!: L.Map;
+  searchQuery: string = '';
 
   private initMap(): void {
     this.map = L.map('map', {
@@ -26,9 +29,29 @@ export class MapComponent implements AfterViewInit {
     tiles.addTo(this.map);
   }
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   ngAfterViewInit(): void {
     this.initMap();
+  }
+
+  onSearch(): void {
+    console.log('Search query:', this.searchQuery);
+    if (!this.searchQuery) {
+      alert('Voer een locatie in!');
+      return;
+    }
+    // Implement search functionality here
+    const url = `https://nominatim.openstreetmap.org/search?q=${this.searchQuery}&format=json&limit=1`;
+    console.log('API URL:', url);
+
+    this.http.get(url).subscribe({
+      next: (Response) => {
+        console.log('API Response:', Response);
+      },
+      error: (error) => {
+        console.error('error', error);
+      },
+    });
   }
 }
